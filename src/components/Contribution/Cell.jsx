5802c2e5f4b730e038d72a520588a484450f8b2b
akkunov <akunov313131@gmail.com>
2023-12-07 17:36:38 +0600

    добавил вывод hover effect checked effect

--- src/components/Contribution/Cell.jsx
+++ src/components/Contribution/Cell.jsx
@@ -5,7 +5,8 @@ const ContributionInfo = (props) => {
     return (
 	<>
 	    <div className={`bg-black w-[150px] h-[42] text-white flex 
-			    flex-col justify-center items-center p-2 rounded-[3px] shadow-2xl`}>
+			    flex-col justify-center items-center p-2 rounded-[3px] shadow-2xl
+			    `}>
 		<span className={`text-white text-xs font-semibold font-inter`}>{contributions}</span>
 		{/*<span className={`text-zinc-500 font-normal`}></span>*/}
 		<span className={`text-zinc-400 font-inter text-[10px] font-medium`}>{date.format('dddd, MMMM D,YYYY ')}</span>
@@ -38,6 +39,8 @@ export const Cell = ({ count, weekIndex}) => {
 	    onMouseLeave={() => mouseLeave()}
 	    className={`w-[15px] h-[15px] rounded-sm m-[2px]
 	    		relative transition-all duration-300
+	    		   hover:border-2 hover:border-zinc-500
+	    		   ${contributionShow ? `border-2 border-zinc-500` : ''}
 		    ${count.contributions > 1  && count.contributions < 9 ? `bg-[#ACD5F2]`
 		: count.contributions > 9  && count.contributions < 19 ? `bg-[#7FA8C9]`
 		    :  count.contributions > 19  && count.contributions < 29 ? `bg-[#527BA0]`
